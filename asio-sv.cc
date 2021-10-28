72f199ea120a93f293bb85d2c278483e443b8e30
Wally Zhao <wallyzhao@gmail.com>
2021-10-28 06:31:25 +0000

    add statistics display

--- asio-sv.cc
+++ asio-sv.cc
@@ -38,6 +38,8 @@
 #include <string>
 #include <fstream>
 #include <thread>
+#include <future>
+#include <memory>
 
 #include <nghttp2/asio_http2_server.h>
 #include "H2Server_Config_Schema.h"
@@ -86,13 +88,17 @@ int main(int argc, char *argv[]) {
     }
     http2 server;
 
+    auto totalReqReceived = std::make_shared<std::atomic<uint64_t>>(0);
+    auto totalMatchedResponseSent = std::make_shared<std::atomic<uint64_t>>(0);
+
     server.num_threads(num_threads);
 
-    server.handle("/", [&](const request &req, const response &res) {
+    server.handle("/", [&, totalReqReceived, totalMatchedResponseSent](const request &req, const response &res) {
 
       static thread_local H2Server h2server(config_schema);
 
       H2Server_Request_Message msg(req);
+      (*totalReqReceived)++;
       auto matched_response = h2server.get_response_to_return(msg);
       if (matched_response)
       {
@@ -115,6 +121,7 @@ int main(int argc, char *argv[]) {
 
           res.write_head(matched_response->status_code, headers);
           res.end(payload);
+          (*totalMatchedResponseSent)++;
       }
       else
       {
@@ -126,6 +133,33 @@ int main(int argc, char *argv[]) {
 
     std::cout<<"addr: "<<addr<<", port: "<<port<<std::endl;
 
+    std::future<void> fu_tps =
+        std::async(std::launch::async, [totalReqReceived, totalMatchedResponseSent]()
+    {
+        uint64_t totalReqReceived_till_lastInterval = 0;
+        uint64_t totalMatchedResponseSent_till_lastInterval = 0;
+        while (true)
+        {
+            auto total_received = totalReqReceived->load();
+            auto total_matched_sent = totalMatchedResponseSent->load();
+            auto delta_received = total_received - totalReqReceived_till_lastInterval;
+            auto delta_matched_sent = total_matched_sent - totalMatchedResponseSent_till_lastInterval;
+            auto now = std::chrono::system_clock::now();
+            auto now_c = std::chrono::system_clock::to_time_t(now);
+            std::cout<<std::endl;
+            std::cout << std::put_time(std::localtime(&now_c), "%c")<<std::endl
+                      << "total req recv: "<<total_received
+                      << ", total response sent: " << total_matched_sent
+                      << ", incoming rps: " << delta_received
+                      << ", outgoing rps: " << delta_matched_sent
+                      << std::endl;
+            totalReqReceived_till_lastInterval = total_received;
+            totalMatchedResponseSent_till_lastInterval = total_matched_sent;
+            
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+    });
+    
     if (config_schema.cert_file.size() && config_schema.private_key_file.size()) {
       boost::asio::ssl::context tls(boost::asio::ssl::context::sslv23);
       tls.use_private_key_file(config_schema.private_key_file, boost::asio::ssl::context::pem);
